8660c2be6bbd32db25416f10d3572b52311f4883
Matt Ray <matthewhray@gmail.com>
2018-08-21 15:20:02 +1000

    Bumped to Chef 14 for sysctl support
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'matt@chef.io'
 license 'Apache 2.0'
 description 'Installs and configures a Chef Automate 2 server'
 long_description 'Installs and configures a Chef Automate 2 server'
-version '0.1.0'
-chef_version '>= 13' if respond_to?(:chef_version)
+version '0.2.0'
+chef_version '>= 14' if respond_to?(:chef_version)
 
 supports 'redhat'
 supports 'centos'
