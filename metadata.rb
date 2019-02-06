9feb625714f2f8e411d86fd83e54888f0402ead2
Matt Ray <matthewhray@gmail.com>
2019-02-06 16:56:06 +1100

    add issues and source URLs for Foodcritic

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,13 @@ maintainer_email 'matt@chef.io'
 license 'Apache-2.0'
 description 'Installs and configures a Chef Automate 2 server'
 long_description 'Installs and configures a Chef Automate 2 server'
-version '0.7.0'
+version '0.7.1'
 chef_version '>= 13' if respond_to?(:chef_version)
 
 supports 'redhat'
 supports 'centos'
 
 depends 'sysctl', '~> 1.0.5'
+
+source_url 'https://github.com/mattray/managed-automate2-cookbook'
+issues_url 'https://github.com/mattray/managed-automate2-cookbook/issues'
