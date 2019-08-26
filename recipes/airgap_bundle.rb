99e029d84a0019aeffed244f5c90e20d174fc014
Matt Ray <matthewhray@gmail.com>
2019-08-26 19:46:36 +1000

    New automate_airgap_bundle custom resource
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- recipes/airgap_bundle.rb
+++ recipes/airgap_bundle.rb
@@ -8,37 +8,9 @@ automate_cli 'download the chef-automate CLI' do
   action :download
 end
 
-# automate_airgap_bundle NAME do
-#   directory # required, probably the NAME
-#   filename # optional if we want to rename it something else
-#   chef-automate # optional different location of the CLI, will build from automate_cli location
-#   action :download
-# end
-
-
-
-# aibdir = node['ma']['aib']['dir']
-# aibfile = aibdir + '/' + node['ma']['aib']['file']
-# aibchef = aibdir + '/chef-automate'
-
-
-# # successful execution of this command produces an Airgap Installation Bundle
-# execute "#{fcpchef} airgap bundle create" do
-#   cwd fcp
-#   # it would be nice to have a guard to only run daily
-# end
-
-# ruby_block "copy new AIB file to #{aibdir}" do
-#   block do
-#     previousaib = shell_out("ls -t1 #{aibdir} | grep [0-9].aib$ | head -1").stdout.strip
-#     newaib = shell_out("ls -t1 #{fcp} | grep [0-9].aib$ | head -1").stdout.strip
-#     puts "\nExisting AIB: #{previousaib}"
-#     puts "New AIB: #{newaib}"
-#     unless newaib.eql?(previousaib)
-#       require 'fileutils'
-#       newfile = fcp + '/' + newaib
-#       FileUtils.cp(newfile, aibdir)
-#       FileUtils.cp(newfile, aibfile)
-#     end
-#   end
-# end
+automate_airgap_bundle 'download the Automate airgap bundle' do
+  directory node['ma']['aib']['dir']
+  chef_automate node['ma']['aib']['dir'] + '/chef-automate'
+  filename node['ma']['aib']['file']
+  action :download
+end
---
+++ resources/automate_airgap_bundle.rb
@@ -0,0 +1,25 @@
+resource_name :automate_airgap_bundle
+
+property :directory, String, name_property: true
+property :chef_automate, String, required: true
+property :filename, String
+
+action :download do
+  destination_dir = new_resource.directory
+  destination_filename = new_resource.filename
+
+  return if destination_filename && ::File.exist?("#{destination_dir}/#{destination_filename}")
+
+  command = "#{new_resource.chef_automate} airgap bundle create"
+  fcp = Chef::Config[:file_cache_path]
+
+  # find the filename from
+  # Success: Your Automate Install Bundle has been written to automate-20190813170406.aib.
+  airgap_filename = shell_out(command, :cwd => fcp).stdout[/automate-\d{14}.aib/]
+
+  destination_filename = airgap_filename unless destination_filename
+
+  execute "cp #{fcp}/#{airgap_filename} #{destination_dir}/#{destination_filename}" do
+    not_if { ::File.exist?("#{destination_dir}/#{destination_filename}") }
+  end
+end
