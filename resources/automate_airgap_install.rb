101a290f8b8ad4ceea94c556bb282dfc9fcc4e2c
Matt Ray <matthewhray@gmail.com>
2019-08-29 17:01:38 +1000

    added support for :upgrade action

--- resources/automate_airgap_install.rb
+++ resources/automate_airgap_install.rb
@@ -25,8 +25,10 @@ action :install do
   if install_url && install_file.nil?
     install_file = fcp + '/chef-automate.aib'
     remote_file install_file do
+      show_progress true
       source install_url
-    end
+      action :nothing
+    end.run_action(:create) # appears to remove filesystem race conditions
   end
 
   # install
@@ -42,20 +44,22 @@ action :restore do
   install_url = new_resource.install_url
   restore_file = new_resource.restore_file
   restore_url = new_resource.restore_url
-
   fcp = Chef::Config[:file_cache_path]
 
-  # is Automate already installed?
-  versions = shell_out("#{chef_automate} version").stdout.split
-  return unless versions[5].nil? # already installed, we're done here
+  # chef-automate status
+  # wait on this?
 
-  # if the install file is not there download it
-  if install_url && install_file.nil?
-    install_file = fcp + '/chef-automate.aib'
-    remote_file install_file do
-      source install_url
-    end
-  end
+  # # is Automate already installed?
+  # versions = shell_out("#{chef_automate} version").stdout.split
+  # return unless versions[5].nil? # already installed, we're done here
+
+  # # if the install file is not there download it
+  # if install_url && install_file.nil?
+  #   install_file = fcp + '/chef-automate.aib'
+  #   remote_file install_file do
+  #     source install_url
+  #   end
+  # end
 
   # let's restore
     # if restore_url && restore_file.nil?
@@ -91,28 +95,31 @@ action :upgrade do
   chef_automate = new_resource.chef_automate
   upgrade_file = new_resource.install_file
   upgrade_url = new_resource.install_url
-  # # if we have an upgrade URL or we have an upgrade file and a directory, it's an upgrade
-  # if node['ma']['upgrade']['url'] || (node['ma']['upgrade']['file'] && node['ma']['upgrade']['dir'])
-  #   upgradeversion = node['ma']['upgrade']['version']
-  #   upgradefile = node['ma']['upgrade']['dir'] + '/' + node['ma']['upgrade']['file']
-  #   upgradeurl = node['ma']['upgrade']['url']
-  #   upgrade = true
-  # end
-  # if upgrade
-  #   log "UPGRADE #{currentversion} to #{upgradeversion}" do
-  #     only_if { upgradeversion.to_i > currentversion.to_i }
-  #   end
-  #   # if the upgrade file is not there download it
-  #   if upgradeurl
-  #     remote_file upgradefile do
-  #       source upgradeurl
-  #       not_if { ::File.exist?(upgradefile) }
-  #     end
-  #   end
-  #   # upgrade
-  #   execute 'chef-automate upgrade run' do
-  #     command "#{chef_automate} upgrade run --airgap-bundle #{upgradefile}"
-  #     cwd fcp
-  #     only_if { upgradeversion.to_i > currentversion.to_i }
-  #   end
+  fcp = Chef::Config[:file_cache_path]
+
+  # if the upgrade file is not there download it
+  if upgrade_url && upgrade_file.nil?
+    upgrade_file = fcp + '/chef-automate-upgrade.aib'
+    remote_file upgrade_file do
+      show_progress true
+      source upgrade_url
+      action :nothing
+    end.run_action(:create) # appears to remove filesystem race conditions
+  end
+
+  # check the version of automate installed
+  current_version = shell_out("#{chef_automate} version").stdout.split.last
+
+  # check to see what version we're trying to upgrade to
+  upgrade_version = shell_out("#{chef_automate} airgap bundle info #{upgrade_file}").stdout.split()[1]
+
+  return unless upgrade_version > current_version
+
+  # upgrade without a current version? Because a nil current_version is < upgrade_version
+  log "UPGRADE #{current_version} to #{upgrade_version}"
+
+  execute 'chef-automate upgrade run' do
+    command "#{chef_automate} upgrade run --airgap-bundle #{upgrade_file}"
+    cwd fcp
+  end
 end
