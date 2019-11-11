da9268ebff779fc460e17cd5811f1a5386ecbef3
Matt Ray <matthewhray@gmail.com>
2019-11-11 17:42:52 +1100

    Fixed test to match backup path set in the attributes
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- test/integration/backup/default_test.rb
+++ test/integration/backup/default_test.rb
@@ -12,17 +12,17 @@ describe directory('/tmp/test/backups') do
   it { should exist }
 end
 
-describe file('/tmp/kitchen/cache/automate-backup.sh') do
+describe file('/var/opt/chef-automate/backups/automate-backup.sh') do
   it { should exist }
   it { should be_executable }
 end
 
 # add crontab entry for cron[chef-automate backup create]
 describe crontab do
-  its('commands') { should include '/tmp/kitchen/cache/automate-backup.sh' }
+  its('commands') { should include '/var/opt/chef-automate/backups/automate-backup.sh' }
 end
 
-describe crontab.commands('/tmp/kitchen/cache/automate-backup.sh') do
+describe crontab.commands('/var/opt/chef-automate/backups/automate-backup.sh') do
   its('minutes') { should cmp '*/5' }
   its('hours') { should cmp '*' }
   its('days') { should cmp '*' }
