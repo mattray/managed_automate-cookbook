148c3f6525d24ec47bf185f13074e097aed65dae
Matt Ray <github@mattray.dev>
2020-05-15 14:44:49 +1000

    Upgraded to Chef 16. Removed Chef 14 support.
    Signed-off-by: Matt Ray <github@mattray.dev>

--- policyfiles/default.rb
+++ policyfiles/default.rb
@@ -7,5 +7,5 @@ default_source :supermarket
 cookbook 'managed_automate', path: '..'
 
 override['ma']['chef-automate'] = '/tmp/test/chef-automate'
-override['ma']['install']['file'] = '/tmp/test/automate-20191024135531.aib'
+override['ma']['install']['file'] = '/tmp/test/automate-20200513205422.aib'
 override['ma']['license']['url'] = 'file://localhost/tmp/test/a2.lic'
